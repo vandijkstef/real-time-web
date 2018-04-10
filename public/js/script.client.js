8481f7ea9bf3e00e007f78901becd3b4f1260545
Stef van Dijk <stef@vandijkstef.nl>
2018-04-11 00:48:37 +0200

    A whole bunch

--- public/js/script.client.js
+++ public/js/script.client.js
@@ -1,6 +1,6 @@
 let ws;
 
-const CatchForms = function() {
+const CatchForms = () => {
 	const forms = document.querySelectorAll('form');
 	forms.forEach((form) => {
 		form.addEventListener('submit', (e) => {
@@ -8,6 +8,7 @@ const CatchForms = function() {
 			switch (e.target.id) {
 			case 'register':
 				console.log('registering new user');
+				ws.send(SocketMessages.register(e.target));
 				break;
 			case 'chatmessage':
 				console.log('Sending chat message');
@@ -21,14 +22,30 @@ const CatchForms = function() {
 	});
 };
 
-const Socket = function() {
+const SetButtons = () => {
+	const buttonRandom = document.querySelector('#register button');
+	if (buttonRandom) {
+		buttonRandom.addEventListener('click', randomizeEmoji);
+		buttonRandom.click();
+	}
+};
+
+const randomizeEmoji = (e) => {
+	e.preventDefault();
+	const emojiSelect = document.querySelector('select#name');
+	if (emojiSelect) {
+		emojiSelect.value = emojiSelect.options[Math.floor(Math.random() * emojiSelect.options.length)].value;
+	}
+};
+
+const Socket = () => {
 	if ('WebSocket' in window) {
 		console.log('Starting Client Websocket');
 		ws = new WebSocket('ws://' + window.location.hostname + ':30005');
-		ws.onopen = function() {
+		ws.onopen = () => {
 			ws.send(SocketMessages.hi());
 		};
-		ws.onmessage = function(e) {
+		ws.onmessage = (e) => {
 			try {
 				// If this doesn't fail, we most likely received wsData from the server - Test all data and see what we need to update
 				const wsData = JSON.parse(e.data);
@@ -37,32 +54,69 @@ const Socket = function() {
 				console.log(e.data);
 			}
 		};
+		ws.onclose = () => {
+			console.log('Socket connection closed, retrying');
+			UIOffline();
+		};
+		ws.onerror = () => {
+			console.log('Woops, we got a Socket error.');
+		};
 	}
 };
 
 const SocketMessages = {
-	hi: function() {
+	hi: () => {
 		return 'HI';
+	},
+	register: (element) => {
+		console.log(element);
+		const value = element.querySelector('select[name=name]').value;
+		return `REGISTER;ELEMENT:${value};`;
 	}
 };
 
-const elements = {
-
+const elements = {};
+const InitUI = () => {
+	CatchForms();
+	SetButtons();
+	if (!elements.new) {
+		elements.new = document.querySelector('#new');
+	}
+	if (!elements.chat) {
+		elements.chat = document.querySelector('#chat');
+	}
+	elements.chat.classList.add('hidden');
 };
 const UpdateFront = (wsData) => {
+	console.log(wsData);
 	if (wsData.clients) {
 		if (!elements.userAmount) {
 			elements.userAmount = document.querySelector('.users .amount');
 		}
 		const clients = Object.keys(wsData.clients).length;
+		elements.userAmount.classList.remove('hidden');
+		if (elements.offline) {
+			elements.offline.classList.add('hidden');
+		}
 		if (elements.userAmount.innerText !== clients.toString()) {
 			elements.userAmount.innerText = clients;
 		}
 	}
 };
-
+const UIOffline = () => {
+	if (!elements.offline) {
+		elements.offline = document.querySelector('section#meta .offline');
+	}
+	if (elements.userAmount) {
+		elements.userAmount.classList.add('hidden');
+	}
+	elements.offline.classList.remove('hidden');
+	setTimeout(() => {
+		Socket();
+	}, 10000);
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-	CatchForms();
+	InitUI();
 	Socket();
 });
